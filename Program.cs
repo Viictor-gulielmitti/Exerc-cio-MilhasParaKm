122de8b382a5a8b04393f4d6a5f353ed175f9079
Viictor-gulielmitti <79719548+Viictor-gulielmitti@users.noreply.github.com>
2021-03-21 21:30:29 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace MILHAS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             string medida;
+             double Milhas;
+
+            Console.WriteLine ( " Digite a medida em milhas: " );
+            medida = Console.ReadLine();
+            Convert.ToDouble ( medida );
+            Milhas = 1609;
+            Console.WriteLine($" A medida em km é: { Milhas }");
+        }
+    }
+}
